d5ddd8e02493e303ac44b133f5cb7b71b0615fde
Nick Walters <nicho.walters@gmail.com>
2019-08-20 16:07:28 +0800

    Added Example SQL Database file
    not completed

---
+++ Projects.sql
@@ -0,0 +1,18 @@
+/*
+CREATE DATABASE information;
+*/
+
+DROP TABLE IF EXISTS Projects;
+CREATE TABLE IF NOT EXISTS Projects (
+	ProjectID INT(11) UNIQUE NOT NULL AUTO_INCREMENT,
+    Title MEDIUMTEXT NOT NULL,
+    Descrip LONGTEXT NOT NULL,
+    Category TEXT, -- Example, category can be Mining, Engineering, Chemical, Software
+    Unit MEDIUMTEXT,
+    studentCapacity INT(11),
+    
+    
+    sponsorUserID INT(11) NOT NULL, -- This is used to link to the Client Table, and get client information if needed
+    
+    PRIMARY KEY(ProjectID)
+);
