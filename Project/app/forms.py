1339617ec5f2c9c5fad8d57326c5b1d1a9e185f3
22257972 <49391931+22257972@users.noreply.github.com>
2019-08-26 14:39:57 +0800

    Remove Old Flask Contents

--- Project/app/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class ProjectRegistrationForm(FlaskForm):
-    title = StringField('Project Title', validators=[DataRequired()])
-    Supervisor1Name = StringField('Primary Supervisor', validators=[DataRequired()])
-    submit = SubmitField('Submit Registration')
