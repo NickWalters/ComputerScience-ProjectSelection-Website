bc3a24974c6f0e0d1dfb42f81363e6dd9be0996e
Nick Walters <nicho.walters@gmail.com>
2019-08-28 20:43:00 +0800

    Added Local Database Support
    Added the configurations for mySQL

--- FinalProject/ProjectProposalSite/ProjectProposalSite/settings.py
+++ FinalProject/ProjectProposalSite/ProjectProposalSite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ProjectProposalSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'information',
+        'USER' : 'root',
+        'PASSWORD' : 'Tso8TFpm',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
