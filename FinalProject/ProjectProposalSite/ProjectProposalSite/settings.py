0735f9192dac176fdfc9d4f7fac2e077163b1e5c
Nick Walters <nicho.walters@gmail.com>
2019-09-11 12:54:13 +0800

    ,
    n

--- FinalProject/ProjectProposalSite/ProjectProposalSite/settings.py
+++ FinalProject/ProjectProposalSite/ProjectProposalSite/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'information',
         'USER' : 'root',
-        'PASSWORD' : 'Gfj123456',
+        'PASSWORD' : 'Tso8TFpm',
         'HOST' : 'localhost',
         'PORT' : '3306',
     }
