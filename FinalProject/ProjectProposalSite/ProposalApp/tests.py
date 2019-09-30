91cfb87dae07015501be89bae6de578a5e2afcfe
Nick Walters <nicho.walters@gmail.com>
2019-09-30 22:36:47 +0800

    Setup tests.py

--- FinalProject/ProjectProposalSite/ProposalApp/tests.py
+++ FinalProject/ProjectProposalSite/ProposalApp/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+# add testing libraries here
+from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your tests here.
+# this class tests the unit models, and will test each class
+
+class userTestCase(TestCase):
+	def setUp(self):
+		# TestCase
+	
+	def userShouldDoThis(self):
+		
