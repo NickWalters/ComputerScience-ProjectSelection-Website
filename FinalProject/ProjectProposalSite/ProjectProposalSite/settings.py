29a2d9fa5a27c525a631926aa6c88262d8b93861
Nick Walters <nicho.walters@gmail.com>
2019-10-18 14:20:15 +0800

    Update settings.py

--- FinalProject/ProjectProposalSite/ProjectProposalSite/settings.py
+++ FinalProject/ProjectProposalSite/ProjectProposalSite/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'information',
         'USER': 'root',
-        'PASSWORD': 'Tso8TFpm',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
