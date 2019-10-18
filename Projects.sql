b75eaba47ea276f0a65b974e70d991f222ea220e
22257972 <49391931+22257972@users.noreply.github.com>
2019-10-18 18:53:32 +0800

    FINAL FINAL Commit

--- Projects.sql
+++ Projects.sql
@@ -1,20 +1,2 @@
-drop database information;
-CREATE DATABASE information;
-USE information;
-
-DROP TABLE IF EXISTS Projects;
-CREATE TABLE IF NOT EXISTS Projects (
-	ProjectID INT(11) UNIQUE NOT NULL AUTO_INCREMENT,
-    Title MEDIUMTEXT NOT NULL,
-    Descrip LONGTEXT NOT NULL,
-    Category TEXT, -- Example, category can be Mining, Engineering, Chemical, Software
-    Unit MEDIUMTEXT,
-    IPID MEDIUMTEXT,
-    CreationDate TIMESTAMP,
-    studentCapacity INT(11),
-    
-    
-    sponsorUserID INT(11) NOT NULL, -- This is used to link to the Client Table, and get client information if needed
-    
-    PRIMARY KEY(ProjectID)
-);
+CREATE DATABASE ProjectPropsalApp;
+USE ProjectPropsalApp;
